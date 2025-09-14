4ae61000ccd9b904a4958be45af20c0de0538fe0
p-gatto <p.gatto@overitech.it>
2025-09-14 10:39:07 +0200

    integration configuration engine

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { ConfigService } from './app/core/config/config.service';
+
+/* bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err)); */
+
+// Bootstrap con precaricamento configurazione
+async function bootstrap() {
+  const app = await bootstrapApplication(App, appConfig);
+  const configService = app.injector.get(ConfigService);
+  await configService.loadConfig();
+  return app;
+}
+
+bootstrap().catch(err => console.error(err));
